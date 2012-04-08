748ad80b54456e0244f7a3236e4c5c389403585e
Nuno Costa <nuno_costa@live.com>
2012-04-08 17:02:04 +0100

    Working on ice reflections

--- HockeySlam/Class/GameEntities/Models/Court.cs
+++ HockeySlam/Class/GameEntities/Models/Court.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 
 namespace HockeySlam.Class.GameEntities.Models
@@ -10,27 +11,40 @@ namespace HockeySlam.Class.GameEntities.Models
 	class Court : BaseModel
 	{
 		Matrix rotation = Matrix.Identity;
-		Effect effect;
-		Game game;
+		Effect _iceEffect;
+		Game _game;
 		Vector3[] diffuseColor;
+		ContentManager _content;
+		GraphicsDevice _graphics;
 
-		public Court(Game game, Camera camera) : base(game, camera)
+		public Court(Game game, Camera camera)
+			: base(game, camera)
 		{
 			model = game.Content.Load<Model>(@"Models\court2");
-			this.game = game;
+			_game = game;
+			_content = _game.Content;
+			_graphics = _game.GraphicsDevice;
+
+			_iceEffect = _game.Content.Load<Effect>(@"Effects\IceEffect");
+			_iceEffect.Parameters["viewportWidth"].SetValue(_graphics.Viewport.Width);
+			_iceEffect.Parameters["viewportHeight"].SetValue(_graphics.Viewport.Height);
+
+
+			
 		}
 
 		public override void LoadContent()
 		{
-			effect = game.Content.Load<Effect>(@"Effects\SimpleEffect");
 			base.LoadContent();
 		}
+		
+
 
 		public override void Draw(GameTime gameTime)
 		{
 			Vector3 diffuseColor;
 			diffuseColor = new Vector3(0.75f, 0.75f, 0.8f);
-			base.DrawEffect(effect, diffuseColor);
+			base.DrawEffect(iceEffect, diffuseColor);
 		}
 
 		public override void Update(GameTime gameTime)
